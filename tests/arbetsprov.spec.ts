dab1d3e3c81b3edcc6e836ecf6668e9f860ba35b
Malin Wetter <malinwetter@gmail.com>
2024-08-19 16:44:40 +0200

    uppgift 2

--- tests/arbetsprov.spec.ts
+++ tests/arbetsprov.spec.ts
@@ -10,3 +10,17 @@ test('Press the button', async ({ page }) => {
   await expect(page.getByText('ASSERTME')).toBeVisible();
 
 });
+
+test('Log in', async ({ page }) => {
+
+    await page.goto('https://daedalus.janniskaranikis.dev/challenges/2-log-in');
+  
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('SafePass123');
+
+    await page.getByRole('button', { name: 'Log in' }).click();
+    
+    await expect(page.locator('div.text-green-600.text-lg')).toContainText('Good Job! Your well earned assert code: ASSERTME' );
+    await expect(page.getByText('ASSERTME')).toBeVisible();
+  
+  });
